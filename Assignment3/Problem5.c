4ce4e79b80112c034c5432f0953835bb6862651f
Sanika-2212 <sanikamisal26@gmail.com>
2026-05-28 00:33:00 +0530

    Add files via upload

---
+++ Assignment3/Problem5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkVowel(char ch)
+{
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+       ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+    {
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter character : ");
+    scanf("%c",&cValue);
+
+    bRet = ChkVowel(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is vowel");
+    }
+    else
+    {
+        printf("It is not vowel");
+    }
+
+    return 0;
+}
